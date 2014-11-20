74e3f6c5980a0142fecdac4bfad3ed0a35975497
Peter-Strbik <peterstrbik97@gmail.com>
2014-11-19 21:47:34 -0500

    Update2
    Added toString() and test for toString() method

---
+++ 03WordSearch/Test.java
@@ -0,0 +1,6 @@
+public class Test{
+    public static void main(String[] args){
+	WordGrid g = new WordGrid(7, 7);
+	System.out.println(g);
+    }
+}
--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -11,14 +11,36 @@ public class WordGrid{
 	data = new char[rows][cols];
         clear();
     }
+    /**Initialize the grid to the default size of 10 by 10 and fill all the
+     *positions with spaces.
+     */
+    public WordGrid(){
+	data = new char[10][10];
+	clear();
+    }
 
     /**Set all values in the WordGrid to spaces ' '*/
     private void clear(){
 	for (int i = 0; i < data.length; i++){
 	    for (int j = 0; j < data[i].length; j++){
-		data[i][j] = ' ';
+		data[i][j] = '_';
 	    }
 	}
+    }
+
+    /**The proper formatting for a WordGrid is created in the toString.
+     *@return a String with each character separated by spaces, and each row
+     *separated by newlines.
+     */
+    public String toString(){
+	String res = "";
+	for (int i = 0; i < data.length; i++){
+	    for (int j = 0; j < data[i].length; j++){
+		res += " " + data[i][j];
+	    }
+	    res += "\n";
+	}
+	return res;
     }
 
 
