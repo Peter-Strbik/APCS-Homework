d2c2962280edcfdff195034dae0793b80b9d54e1
Peter-Strbik <peterstrbik97@gmail.com>
2014-11-20 12:14:57 -0500

    Update 4

--- 03WordSearch/Test.java
+++ 03WordSearch/Test.java
@@ -9,5 +9,9 @@ public class Test{
 	System.out.println(g);
 	g.addWordHorizontal("WHAT", 8, 8);
 	System.out.println(g);
+	g.addWordVertical("COMPSCI", 3, 1);
+	System.out.println(g);
+	g.addWordVertical("SHREKD", 4, 7);
+	System.out.println(g);
     }
 }
