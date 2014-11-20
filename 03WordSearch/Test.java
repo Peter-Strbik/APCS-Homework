c702b0bf831cd4f11c5e771a32367cc4da34002b
Peter-Strbik <peterstrbik97@gmail.com>
2014-11-19 22:37:03 -0500

    Update3
    Added addWordHorizontal() and tests

--- 03WordSearch/Test.java
+++ 03WordSearch/Test.java
@@ -1,6 +1,13 @@
 public class Test{
     public static void main(String[] args){
-	WordGrid g = new WordGrid(7, 7);
+	WordGrid g = new WordGrid(12, 12);
+	System.out.println(g);
+	g.addWordHorizontal("AMAZING", 3, 4);
+	g.addWordHorizontal("HELLO", 7, 6);
+	System.out.println(g);
+	g.addWordHorizontal("WHAT", 3, 4);
+	System.out.println(g);
+	g.addWordHorizontal("WHAT", 8, 8);
 	System.out.println(g);
     }
 }
