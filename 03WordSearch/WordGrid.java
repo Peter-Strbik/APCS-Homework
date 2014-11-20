6fc4b6fc1f24c785b4e3fd3719e68f5415a0f63b
Peter-Strbik <peterstrbik97@gmail.com>
2014-11-19 21:28:23 -0500

    Update
    Added Constructor and clear() function

--- 03WordSearch/WordGrid.java
+++ 03WordSearch/WordGrid.java
@@ -2,5 +2,24 @@ public class WordGrid{
     private char[][]data;
 
 
+    /**Initialize the grid to the size specified and fill all of the positions
+     *with spaces.
+     *@param row is the starting height of the WordGrid
+     *@param col is the starting width of the WordGrid
+     */
+    public WordGrid(int rows, int cols){
+	data = new char[rows][cols];
+	data.clear();
+    }
+
+    /**Set all values in the WordGrid to spaces ' '*/
+    private void clear(){
+	for (int i = 0; i < data.length; i++){
+	    for (int j = 0; j < data[i].length; j++){
+		data[i][j] = " ";
+	    }
+	}
+    }
+
 
 }
