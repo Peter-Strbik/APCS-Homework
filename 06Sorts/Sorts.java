cbb9b2e3f04ead706e389353a40c6a7901bd02b0
Peter-Strbik <peterstrbik97@gmail.com>
2014-12-16 22:29:38 -0500

    Update
    Added insertion and selection sorts to file, changed them to sort
    integers.

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -7,9 +7,9 @@ public class Sorts{
 	for (int i = 0; i < x.length; i++){
 	    x[i] = r.nextInt(100);
 	}
-	System.out.println(x);
-	BubbleSort(x);
-	System.out.println(x);
+	System.out.println(printArray(x));
+        insertionSort(x);
+	System.out.println(printArray(x));
 
     }
 
@@ -29,6 +29,43 @@ public class Sorts{
 		}
 	    }
 	}
+    }
+
+    public static void selectionSort(int[] L){
+	for (int i = 0; i < L.length; i++){
+	    int pos = i;
+	    int small = L[i];
+	    int indexSwitch = i;
+	    for (int j = i+1; j < L.length; j++){
+		if (L[j] < small){
+		    small = L[j];
+		    indexSwitch = j;
+		}
+	    }
+	    L[indexSwitch] = L[i];
+	    L[i] = small;
+	}   
+    }
+
+    public static void insertionSort(int[] L){
+        for (int i = 0; i < L.length; i++){
+	    int temp = L[i];
+	    int pos = i;
+	    while(pos > 0 && temp < L[pos - 1]){
+		L[pos] = L[pos - 1];
+		pos--;
+	    }
+	    L[pos] = temp;
+	}
+    }
+
+    public static String printArray(int[] x){
+	String res = "[ ";
+	for (int i = 0; i < x.length; i++){
+	    res += x[i] + " ";
+	}
+	res += "]";
+	return res;
     }
 
 }
