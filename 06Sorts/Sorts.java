0e864d82324e2521abb3f8777f193782fdc7ad7a
Peter-Strbik <peterstrbik97@gmail.com>
2014-12-21 17:48:12 -0500

    Update
    Added Radix Sort / General testing

--- 06Sorts/Sorts.java
+++ 06Sorts/Sorts.java
@@ -3,14 +3,52 @@ public class Sorts{
 
     public static void main(String[] args){
 	Random r = new Random();
-	int[] x  = new int[100];
+	int[] x  = new int[100000];
 	for (int i = 0; i < x.length; i++){
-	    x[i] = r.nextInt(100);
+	    x[i] = r.nextInt(100000);
 	}
-	System.out.println(printArray(x));
-        insertionSort(x);
-	System.out.println(printArray(x));
 
+	long Start, End, timeTaken;
+	
+	int[] x1 = x.clone();
+	System.out.println("Insertion:");
+	Start = System.currentTimeMillis();
+	insertion(x1);
+	End = System.currentTimeMillis();
+	timeTaken = End - Start;
+	System.out.println(timeTaken + "\n");
+
+	int[] x2 = x.clone();
+	System.out.println("Selection:");
+	Start = System.currentTimeMillis();
+	selection(x2);
+	End = System.currentTimeMillis();
+	timeTaken = End - Start;
+	System.out.println(timeTaken + "\n");
+
+	int[] x3 = x.clone();
+	System.out.println("Bubble:");
+	Start = System.currentTimeMillis();
+	bubble(x3);
+        End = System.currentTimeMillis();
+        timeTaken = End - Start;
+	System.out.println(timeTaken + "\n");
+	
+	int[] x4 = x.clone();
+	System.out.println("Radix:");
+        Start = System.currentTimeMillis();
+	radix(x4);
+        End = System.currentTimeMillis();
+        timeTaken = End - Start;
+	System.out.println(timeTaken + "\n");
+
+	int[] x5 = x.clone();
+	System.out.println("Arrays Sort:");
+        Start = System.currentTimeMillis();
+	Arrays.sort(x5);
+        End = System.currentTimeMillis();
+        timeTaken = End - Start;
+	System.out.println(timeTaken + "\n");
     }
 
     public static String name(){
@@ -22,15 +60,35 @@ public class Sorts{
     }
 
     public static void radix(int[] L){
-	ArrayList<ArrayList<Integer>> buckets = new ArrayList<ArrayList<Integer>>(10);
+	ArrayList<ArrayList<Integer>> buckets = new ArrayList<ArrayList<Integer>>();
+	for (int wot = 0; wot < 10; wot++){
+	    buckets.add(new ArrayList<Integer>());
+	}
+	
 	int maxDigits = 0;
 	for (int i = 0; i < L.length; i++){
 	    if ((int)(Math.log10(L[i])+1) > maxDigits){
-		maxDigits = (int)(Math.log10(n)+1);
+		maxDigits = (int)(Math.log10(L[i])+1);
 	    }
 	}
+	
 	for (int i = 0; i < maxDigits; i++){
-	}
+	    int div =(int)(Math.pow(10, i));
+
+	    for (int j = 0; j < L.length; j++){
+	        int place = (L[j] / div) % 10;
+		buckets.get(place).add(L[j]);
+	    }
+
+	    int temp = 0;
+	    for (int k = 0; k < buckets.size(); k++){
+		for (int m = 0; m < buckets.get(k).size(); m++){
+		    L[temp] = buckets.get(k).get(m);
+		    temp++;
+		}
+		buckets.get(k).clear();
+	    }
+	}                		
     }
 	
 
